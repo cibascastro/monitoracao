2c20e21d9097a440653927396497dcbf5b533eb7
Cibelle Castro <38285682+cibascastro@users.noreply.github.com>
2024-11-25 18:30:02 -0300

    Create main.tf
    Parte 1 - Primeiro script Terraform

---
+++ main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.11.0"
+    }
+  }
+}
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+
+resource "docker_image" "postgres" {
+  name = "postgres:latest"
+}
+
+resource "docker_container" "postgres" {
+  image = docker_image.postgres.latest
+  name  = var.container_name
+
+  ports {
+    internal = var.postgres_port
+    external = var.postgres_port
+  }
+
+  env = [
+    "POSTGRES_USER=${var.postgres_user}",
+    "POSTGRES_PASSWORD=${var.postgres_password}"
+  ]
+}
