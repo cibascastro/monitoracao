f4bdc8294bac6833d03e61e406dd2ade5b1d6bb7
Cibelle Castro <38285682+cibascastro@users.noreply.github.com>
2024-11-25 23:50:03 -0300

    ajustando para criar o terraform.tfvars

--- variables.tf
+++ variables.tf
@@ -1,23 +1,19 @@
 variable "container_name" {
   description = "Name of the Postgres container"
   type        = string
-  default     = "postgres-container"
 }
 
 variable "postgres_port" {
   description = "Port for Postgres"
   type        = number
-  default     = 5433
 }
 
 variable "postgres_user" {
   description = "Postgres user"
   type        = string
-  default     = "postgres_sa"
 }
 
 variable "postgres_password" {
   description = "Postgres password"
   type        = string
-  default     = "Asd@1234@zxc"
 }
